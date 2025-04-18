e05301393ea45b36b991cc525cf57822f3cecd76
mrgopalsharma <72728990+mrgopalsharma@users.noreply.github.com>
2025-04-18 09:19:12 +0530

    Add files via upload

---
+++ OddEven.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter number: "))
+if (num1 % 2 ==0):
+    print (num1, "is an Even Number")
+
+else:
+    print (num1,"is an Odd Number")
---
+++ sumofint.py
@@ -0,0 +1,5 @@
+totalsum=0
+for i in range (0,51):
+    totalsum += i
+
+print("Total sum of number from 0 to 50 is: ", totalsum)
